0ceb43e03fd16c8466f1e10dfe88e7d60b75d0cb
shashank <shashank99928@gmail.com>
2021-02-17 23:30:37 +0530

    added images,size

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -30,6 +30,7 @@ class Directory extends Component {
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
+          size:"large",
           linkUrl: "shop/womens",
         },
         {
@@ -37,6 +38,7 @@ class Directory extends Component {
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
+          size:"large",
           linkUrl: "shop/mens",
         },
       ],
@@ -45,8 +47,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map((section) => (
-          <MenuItem key={section.id} title={section.title} image={section.imageUrl} />
+        {this.state.sections.map(({title,id,imageUrl,size}) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
         ))}
       </div>
     );
