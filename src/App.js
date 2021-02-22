fe5b6a8f5ce78b047d1ec3a84eaac4fb15a7c142
shashank <shashank99928@gmail.com>
2021-02-22 13:35:04 +0530

    commit added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import {Route} from 'react-router-dom'
 import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = ()=>(
+  <div>
+  HAts page
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Homepage />
+        <Route exact path='/' component={Homepage}/>
+        <Route  path='/hats' component={HatsPage} />
       </div>
     );
   }
