3a28284e6b861c2003030cad50e4c9d089eb33da
shashank <shashank99928@gmail.com>
2021-02-25 16:47:58 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
-import {Route} from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 
-const HatsPage = ()=>(
-  <div>
-  HAts page
-  </div>
-)
+const HatsPage = () => <div>HAts page</div>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={Homepage}/>
-        <Route  path='/hats' component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
